a5d5ba17da28ab870f1d4a501df80615243fb1ce
Wr2020 <comin_008@163.com>
2020-05-19 12:04:47 +0800

    封装自己的数组

--- src/Array.java
+++ src/Array.java
@@ -1,2 +1,42 @@
 public class Array {
+
+    private int[] data;
+    private int size;
+
+    /**
+     * 构造函数，传入数组容量capacity构造Array
+     * @param capacity 数组容量
+     */
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    /**
+     *  默认容量为10
+     */
+    public Array() {
+        this(10);
+    }
+
+    /**
+     * @return 获取数组种元素个数
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @return 获取数组容量
+     */
+    public int getCapacity() {
+        return data.length;
+    }
+
+    /**
+     * @return 判断数组元素是否为0
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
 }
