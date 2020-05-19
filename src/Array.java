0fd2526a3e2b4aaeb048a2d5bc3c43d3f6fa75fd
Wr2020 <comin_008@163.com>
2020-05-19 12:25:48 +0800

    向数组中添加元素

--- src/Array.java
+++ src/Array.java
@@ -39,4 +39,42 @@ public class Array {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    /**
+     * 向数组末尾添加元素
+     * @param e
+     */
+    public void addLast(int e) {
+
+       add(size, e);
+    }
+
+    /**
+     * 向数组开始添加元素
+     * @param e
+     */
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    /**
+     * 在index位置插入元素e
+     * @param index 位置
+     * @param e 元素
+     */
+    public void add(int index, int e) {
+
+        if (size == data.length) {
+            throw new IllegalArgumentException("add failed. Array is full");
+        }
+
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("add failed. Require index >= 0 and index <= size.");
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+    }
 }
